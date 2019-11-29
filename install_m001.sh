eac800e11b1ec77048fca20f7297160bac9d78b8
php-aptech <56375206+php-aptech@users.noreply.github.com>
2019-11-29 15:56:05 +0700

    Update install_m001.sh

--- install_m001.sh
+++ install_m001.sh
@@ -2,17 +2,20 @@
 date=`date +%Y%m%d`
 dir=/root/Scripts
 
+######Disable Selinux
 DropSelinux()
 {
 echo "SELINUX=disabled" > /etc/selinux/config; echo "SELINUXTYPE=targeted" >> /etc/selinux/config; setenforce 0
 }
 
+######Add Port Iptables "3122,10050"
 AddPortIptables()
 {
 cp $dir/iptables.txt /etc/sysconfig/iptables
 service iptables restart
 }
 
+######Config Port SSH port 3122
 ConfigSSH()
 {
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.$date
@@ -21,6 +24,7 @@ cp $dir/config_ssh.txt /etc/ssh/sshd_config
 service sshd restart
 }
 
+######Config monitor zabbix agent
 Zabbix_Agent()
 {
 rpm --import http://repo.zabbix.com/RPM-GPG-KEY-ZABBIX
@@ -29,19 +33,24 @@ yum install zabbix-agent -y
 #cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.$date
 echo > /etc/zabbix/zabbix_agentd.conf
 cp $dir/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
-echo "Vui long nhap IP Zabbix_Client can monitor: "
+echo "Vui lòng nhập vào IP cần monitor: "
 read ip_zabbix
 echo "Hostname=$ip_zabbix" >> /etc/zabbix/zabbix_agentd.conf 
 service zabbix-agent start
 chkconfig zabbix-agent on
 }
 
+######Change password root
 Change_Pass()
 {
 #echo "linuxpassword" | passwd --stdin linuxuser
 echo "tKdVd#%GVfIp*1ey#&Wx" | passwd --stdin root
 }
 
+
+######################
+#	 Run Function	 #	
+######################
 Change_Pass
 DropSelinux
 AddPortIptables
