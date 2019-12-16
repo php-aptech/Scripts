cd24c8091643cc2129b24daf27894db17ee03b4a
php-aptech <56375206+php-aptech@users.noreply.github.com>
2019-12-16 14:36:47 +0700

    Update install_m001_Centos7.sh

--- install_m001_Centos7.sh
+++ install_m001_Centos7.sh
@@ -73,10 +73,10 @@ echo "tKdVd#%GVfIp*1ey#&Wx" | passwd --stdin root
 ######################
 #	 Run Function	 #	
 ######################
-VntimeZone
+#VntimeZone
 ConfigIptables
-Change_Pass
 DropSelinux
 AddPortIptables
 ConfigSSH
 Zabbix_Agent
+Change_Pass
