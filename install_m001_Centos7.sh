bfd5c8df4b1001cf8e0577c80045cc215c972d9f
php-aptech <56375206+php-aptech@users.noreply.github.com>
2020-03-18 11:40:33 +0700

    Update install_m001_Centos7.sh

--- install_m001_Centos7.sh
+++ install_m001_Centos7.sh
@@ -56,7 +56,7 @@ touch .ssh/authorized_keys
 chmod 700 .ssh
 chmod 600 .ssh/authorized_keys
 # Coppy Key ssh
-cp  /$dir/public_key.txt /home/tech/.ssh/authorized_keys
+cp  $dir/public_key.txt /home/tech/.ssh/authorized_keys
 }
 
 ######Add Key SSH To File Authorized_key
